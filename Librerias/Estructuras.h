677c7ee37619d8b47166cf6ceeb3788d087aa8f6
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-05-28 14:32:52 -0400

    Co-authored-by: Daniel Castellanos <DanCas03@users.noreply.github.com> Co-authored-by: morochobrandon <morochobrandon@users.noreply.github.com>

--- Librerias/Estructuras.h
+++ Librerias/Estructuras.h
@@ -17,18 +17,20 @@ typedef struct tVehiculo
     string nombreEspanol;
     string nombreIngles;
     string conductores;
-    int tipoCaucho;
-    int tamanoCaucho;
-    int velocidad;
+    string tipoCaucho[3]={"Normales","Anti coleo","Todo terreno"};
+    string tamanoCaucho[3]={"Pegado al piso","Normales","Monster truck"};
+    string velocidad[4]= {"Lento","Normal","Rapido","Ultra rapido"};
    // string tiempoDisminucion;
     tResistencia resistencia;
     clock_t cronometro;
     int PosicionDeLlegada;
+    char vehiculoEnPantalla[11] = {'¥','@', '©', '£', 'Ç','$','Ø', 'æ', 'þ', '§', '®'}; // @ = Vehiculo, O = Vehiculo con daño, X = Vehiculo destruido
 };
 typedef struct tObstaculo
 {
     string nombre;
     tResistencia resistencia;
+    char display[3] = {'¤', '¶', '#'};
     // int tipo;
     // int velocidad;
     // int tiempoDisminucion;
@@ -44,8 +46,6 @@ typedef struct tKilometro
 typedef struct tCarril
 {
     tKilometro *inicio;
-    tKilometro *fin;
-    tVehiculo vehiculo;
     tCarril *prox;
     tKilometro *ubicacionVehiculo;
     int numeroKilometros;
