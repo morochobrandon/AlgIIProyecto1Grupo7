21ac85212cb69caba8e89713defb77700a920398
David Eduardo Hidalgo Varela <dehidalgo.22@est.ucab.edu.ve>
2023-06-10 20:18:25 -0400

    update TDAPista.h

--- Librerias/TDAPista.h
+++ Librerias/TDAPista.h
@@ -4,8 +4,18 @@
 #include <iostream>
 #include <windows.h>
 #include <fstream>
+#include "GestionVehículos.h"
 
 using namespace std;
+
+tPista *crearPista();
+void mostrarPista(tPista pista);
+
+void vehiculoEnPista(tPista *pista, tVehiculo vehiculo);
+void cargarPista(tPista *pista, ifstream& archivo);
+void guardarPista(tPista *pista, ofstream& archivo);
+void cargarPista(tPista *pista, ifstream& archivo);
+void destruirPista(tPista *pista);
 
 
 #include "TDAPista.cpp"
